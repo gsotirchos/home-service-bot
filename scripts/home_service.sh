adfe28a024b01c8abf2cc8ef237476c63ea9a602
7555G <630r63.7555@gmail.com>
2021-10-31 21:58:19 +0200

    Minor changes

--- scripts/home_service.sh
+++ scripts/home_service.sh
@@ -7,7 +7,7 @@ trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 killall roscore rosmaster
 
 # function to open an xterm window, execute a command, and wait for a node to be brought up
-xterm_run() {
+xterm_exec() {
     cmd="$1"
     node="$2"
 
@@ -22,27 +22,27 @@ xterm_run() {
 
 # launch Gazebo world
 echo -n "Launching Gazebo... "
-xterm_run "roslaunch turtlebot_gazebo turtlebot_world.launch" "/gazebo"
+xterm_exec "roslaunch mapping_bot world.launch" "/gazebo"
 echo "DONE"
 
-# launch AMCL and wait for it
-echo -n "Launching AMCL... "
-xterm_run "roslaunch turtlebot_gazebo amcl_demo.launch" "/amcl"
+# launch RTAB-Map localization
+echo -n "Launching RTAB-Map localization... "
+xterm_exec "roslaunch mapping_bot localization.launch rviz:=false move_base:=true" "/rtabmap"
 echo "DONE"
 
 # launch RViz with configuration
 echo -n "Launching RViz... "
-xterm_run "rosrun rviz rviz -d $(rospack find home_service_bot)/../rviz_config/turtlebot_config.rviz" "/rviz"
+xterm_exec "rosrun rviz rviz -d $(rospack find home_service_bot)/../rviz_config/config.rviz" "/rviz"
 echo "DONE"
 
-# start pick_objects node and wait for it
+# start the pick_objects node
 echo -n "Starting the pick_objects node... "
-xterm_run "rosrun pick_objects pick_objects" "/pick_objects"
+xterm_exec "rosrun pick_objects pick_objects" "/pick_objects"
 echo "DONE"
 
-# start add_markers node and wait for it
+# start the add_markers node
 echo -n "Starting the add_markers node... "
-xterm_run "rosrun add_markers add_markers" "/add_markers"
+xterm_exec "rosrun add_markers add_markers" "/add_markers"
 echo "DONE"
 
 echo "Press Ctrl+C to close everything"
